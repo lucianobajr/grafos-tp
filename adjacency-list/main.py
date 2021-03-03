40559c298f18059ee08e0d4bf46faebeec07e4b6
Isabella Ramos <isabellamenezesramos@outlook.com>
2021-03-03 11:43:34 -0300

    adicionando entrada por arquivo e tamanho do grafo

--- adjacency-list/main.py
+++ adjacency-list/main.py
@@ -4,7 +4,29 @@ class AdjNode:
         self.weight = weight  
         self.next = None
   
-  
+
+#def new_graph(size):
+#    ref_arquivo = open("entrada01.txt","r")
+#    V = int(ref_arquivo.readline())
+#    graph = Graph(V)
+
+#    for linha in (ref_arquivo):
+#        size = size_graphAux(size)
+#        valores = linha.split()
+#        Value01 = int(valores[0])
+#        Value02 = int(valores[1])
+#        Weight = float(valores[2])
+#        graph.add_edge(Value01,Value02,Weight)
+
+#   return size
+    
+def size_graphAux(size):
+    size +=1
+    return size
+
+def print_size(size):
+    print("O tamanho do grafo é ",size)
+
 # A class to represent a graph. A graph 
 # is the list of the adjacency lists. 
 # Size of the array will be the no. of the 
@@ -36,21 +58,39 @@ class Graph:
                 print(" -- {} W: {} || ".format(temp.vertex, temp.weight), end="") 
                 temp = temp.next
             print(" \n") 
-  
-  
+    
+    
 # Driver program to the above graph class 
 if __name__ == "__main__": 
     
-    V = 5
-    graph = Graph(V) 
-    graph.add_edge(0, 1,1) 
-    graph.add_edge(0, 4,2) 
-    graph.add_edge(1, 2,3) 
-    graph.add_edge(1, 3,4) 
-    graph.add_edge(1, 4,2) 
-    graph.add_edge(2, 3,5) 
-    graph.add_edge(3, 4,6) 
-  
+    size = 0
+    ref_arq = open("entrada01.txt","r")
+    V = int(ref_arq.readline())
+    graph = Graph(V)
+
+    for linha in (ref_arq):
+        size = size_graphAux(size)
+        Values = linha.split()
+        Value01 = int(Values[0])
+        Value02 = int(Values[1])
+        Weight = float(Values[2])
+        graph.add_edge(Value01,Value02,Weight)
+    
+    print_size(size)
     graph.print_graph()
+
+    #V = 5
+    #graph = Graph(V) 
+    #graph.add_edge(0, 1,1) 
+    #graph.add_edge(0, 4,2) 
+    #graph.add_edge(1, 2,3) 
+    #graph.add_edge(1, 3,4) 
+    #graph.add_edge(1, 4,2) 
+    #graph.add_edge(2, 3,5) 
+    #graph.add_edge(3, 4,6) 
+    #graph new_graph()
+    #graph.print_graph()
+    
+    #graph.size_graph()
   
 # This code is contributed by Kanav Malhotra 
