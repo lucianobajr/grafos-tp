45b810f00d17a614f78fa9c2843e84bec8aba812
PitanoGit <46332885+PitanoGit@users.noreply.github.com>
2021-03-23 16:56:45 -0300

    conexidade e ponte add - ok

--- componentes conexas e  ponte/main.py
+++ componentes conexas e  ponte/main.py
@@ -13,6 +13,7 @@ class Graph:
         self.V = vertices 
         self.numConectedComp = 0
         self.graph = [None] * self.V 
+        self.Time = 0
   
     # Function to add an edge in an undirected graph  
     # é melhor chamar de neighbor por ser um grafo nao direcionado
@@ -57,8 +58,7 @@ class Graph:
             if visited[v] == False:
                 temp = []
                 self.numConectedComp+=1
-                cc.append(self.DFSUtil(temp, v, visited))
-                
+                cc.append(self.DFSUtil(temp, v, visited))                
         return cc
 
     def DFSUtil(self, temp, v, visited):
@@ -83,6 +83,71 @@ class Graph:
                 
         return temp
 
+    def connectedComponents(self):
+        visited = []
+        cc = []
+        for i in range(1,self.V+1):
+            visited.append(False)
+        for v in range(1,self.V):
+            if visited[v] == False:
+                temp = []
+                self.numConectedComp+=1
+                cc.append(self.DFSUtil(temp, v, visited))
+                
+        return cc
+
+
+    # DFS based function to find all bridges. It uses recursive
+   
+    def bridgeUtil(self,u, visited, parent, low, disc):
+  
+        # Mark the current node as visited and print it
+        visited[u]= True
+  
+        # Initialize discovery time and low value
+        disc[u] = self.Time
+        low[u] = self.Time
+        self.Time += 1
+  
+        #Recur for all the vertices adjacent to this vertex
+        aux = self.graph[u]
+        while aux:
+            # If v is not visited yet, then make it a child of u
+            # in DFS tree and recur for it
+            if visited[aux.vertex] == False :
+                parent[aux.vertex] = u
+                self.bridgeUtil(aux.vertex, visited, parent, low, disc)
+  
+                # Check if the subtree rooted with v has a connection to
+                # one of the ancestors of u
+                low[u] = min(low[u], low[aux.vertex])  
+  
+                ''' If the lowest vertex reachable from subtree
+                under v is below u in DFS tree, then u-v is
+                a bridge'''
+                if low[aux.vertex] > disc[u]:
+                    print ("%d %d" %(u,aux.vertex))                    
+                    #aux = aux.next
+                      
+            elif aux.vertex != parent[u]: # Update low value of u for parent function calls.
+                low[u] = min(low[u], disc[aux.vertex])
+            aux = aux.next
+    def bridge(self):
+    
+        # Mark all the vertices as not visited and Initialize parent and visited, 
+            # and ap(articulation point) arrays
+        visited = [False] * (self.V)
+        disc = [float("Inf")] * (self.V)
+        low = [float("Inf")] * (self.V)
+        parent = [-1] * (self.V)
+    
+        # Call the recursive helper function to find bridges
+        # in DFS tree rooted with vertex 'i'
+        for i in range(self.V):
+            if visited[i] == False:
+                self.bridgeUtil(i, visited, parent, low, disc)
+
+
 def size_graphAux(size):
     size +=1
     return size
@@ -94,7 +159,7 @@ def print_size(size):
 # Driver program to the above graph class 
 if __name__ == "__main__": 
     size = 0
-    with open("teste.txt", 'r') as file_input:  # arquivo de input
+    with open("teste3.txt", 'r') as file_input:  # arquivo de input
         V = file_input.readline()
         graph = Graph(int(V)+1) 
         while True:
@@ -112,10 +177,13 @@ if __name__ == "__main__":
    
     graph.print_graph()
     print_size(size)
-    
-    
+        
     cc = graph.connectedComponents()
     print("Following are connected components")
     print(cc)
-
+    print("entrou eponte")
     print(graph.numConectedComp)
+
+    ePonte = graph.bridge()
+    
+    
