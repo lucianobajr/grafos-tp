b8b7309cf589dc6a1d74a2cd2cfaccdae9a70c02
Guilherme Aguiar Silva Milanez <guigasilva27@gmail.com>
2021-03-23 14:57:58 -0300

    com visualizador

--- src/main.py
+++ src/main.py
@@ -1,16 +1,20 @@
 import sys
+import networkx as nx 
+import matplotlib.pyplot as plt   
 
 class AdjNode: 
     def __init__(self, data,weight): 
         self.vertex = data 
         self.weight = weight  
-        self.next = None
-    
+        self.next = None 
+        self.marked = False # use in DFS  
+        self.explored = False
+     
 
 class Graph: 
     def __init__(self, vertices): 
         self.V = vertices 
-
+        self.G = nx.Graph()
         self.graph = [None] * self.V 
   
     # Function to add an edge in an undirected graph  
@@ -24,7 +28,11 @@ class Graph:
 
         node = AdjNode(Neighbor1,weight) 
         node.next = self.graph[Neighbor2] 
-        self.graph[Neighbor2] = node 
+        self.graph[Neighbor2] = node  
+
+        self.G.add_node(Neighbor1) 
+        self.G.add_node(Neighbor2) 
+        self.G.add_edge(Neighbor1,Neighbor2,weight=weight)
   
     # Function to print the graph 
     def print_graph(self): 
@@ -45,14 +53,87 @@ class Graph:
                 if(temp.vertex == ver):
                     degree+=1
                 temp = temp.next
-        return degree
-                            
-  
-# Driver program to the above graph class 
+        return degree 
+#_________________________________________________________________________________#    
+    def Neighbor_by_vertex(self, vertex): 
+        findFlag = False
+        for i in range(self.V): 
+            if i == vertex:
+                print("Lista de vizinhos do vertice {}\n ".format(i), end="") 
+                temp = self.graph[i] 
+                findFlag = True
+                while temp: 
+                    print(" -- {} W: {} || ".format(temp.vertex, temp.weight), end="") 
+                    temp = temp.next
+                print(" \n")  
+        if findFlag == False:
+            print("O vertice digitado nao possui vizinhos no grafo inserido\n")
+#_________________________________________________________________________________#
+    def Unmark_All(self):  
+        for i in range (0,self.V):
+            temp = self.graph[i] 
+            while temp:  
+                temp.marked = False    
+                temp.explored = False
+                temp = temp.next
+ #_________________________________________________________________________________#
+    def Intern_DFS(self, vertex):          
+        temp = self.graph[vertex] 
+        while temp:  
+            if temp.marked == False:  
+                temp.explored = True   
+                temp.marked = True
+                print("{} - {}".format(vertex, temp.vertex)) 
+                aux_temp = self.graph[temp.vertex]  
+                while aux_temp:  
+                    if aux_temp.vertex == vertex: 
+                        aux_temp.marked = True
+                    aux_temp = aux_temp.next
+            
+                self.Intern_DFS(temp.vertex)
+            else:   
+                if temp.explored == False:  
+                    #file = open('aresta_retorno.txt','w')  
+                    arq=open("teste.txt","w")
+                    if temp.explored == True :  
+                        #aux = '{} - {}\n'.format(vertex,temp.vertex)
+                        #print("{} - {}".format(vertex, temp.vertex)) 
+                        arq.write("%d %d\n"%(vertex,temp.vertex))
+                    arq.close()
+                        #file.write(aux) 
+                        #file.close()
+
+            temp = temp.next 
+
+
+
+ #_________________________________________________________________________________#    
+    def DFS(self, vertex):   
+        
+        self.Unmark_All()  
+       # self.graph[vertex].marked = True
+        self.Intern_DFS(vertex) 
+
+def size_graphAux(size):
+    size +=1
+    return size
+
+def print_size(size):
+    print("O tamanho do grafo é", size) 
+
+def NbV():  
+    while True:
+        vertex = int(input("Entre com vertice (-1 para sair): ")) 
+        if vertex == -1 : 
+            break
+        graph.Neighbor_by_vertex(vertex)
+
+#______________________________________Driver___________________________________________                        
 if __name__ == "__main__": 
+    size = 0
     with open(str(sys.argv[2]), 'r') as file_input:  # arquivo de input
         V = file_input.readline()
-        graph = Graph(int(V)) 
+        graph = Graph(int(V)+1) 
         while True:
             try:
                 file_line = file_input.readline()
@@ -61,32 +142,28 @@ if __name__ == "__main__":
                 else:
                     item = file_line.split(" ")
                     graph.add_edge(int(item[0]),int(item[1]),int(item[2]))
+                    size = size_graphAux(size)
             except:
                 print("Erro ao inserir Aresta")
            
    
-    graph.print_graph()
-
-
-    '''
-    V= 5
-    graph = Graph(V) 
-    
-    graph.add_edge(0,1,1) 
-    graph.add_edge(0,4,2) 
-    graph.add_edge(1,2,3) 
-    graph.add_edge(1,3,4) 
-    graph.add_edge(1,4,2) 
-    graph.add_edge(2,3,5) 
-    graph.add_edge(3,4,6) 
-
-    graph.print_graph()
-
-    print("Vertice 0 tem grau {}".format(graph.findDegree(0)))
-    print("Vertice 1 tem grau {}".format(graph.findDegree(1)))
-    print("Vertice 2 tem grau {}".format(graph.findDegree(2)))
-    print("Vertice 3 tem grau {}".format(graph.findDegree(3)))
-    print("Vertice 4 tem grau {}".format(graph.findDegree(4)))
-
-    # This code is contributed by Kanav Malhotra 
-    '''
+    #graph.print_graph()
+    print_size(size) 
+    NbV()  
+    vertex = int(input("Entre com vertice (DFS): "))
+    #chama busca profunda 
+    graph.DFS(vertex)  
+     #garante a posicao randomica 
+    pos=nx.random_layout(graph.G) 
+    #define resolucao da imagem 
+    fig = plt.figure(figsize=(40, 40), dpi= 70)  
+    #nao haver sobreposicao
+    plt.clf() 
+    #parametros do desenho
+    nx.draw_networkx(graph.G,pos,node_color='yellowgreen') 
+    #colocar peso nas arestas 
+    labels = nx.get_edge_attributes(graph.G,'weight') 
+    # desenha 
+    nx.draw_networkx_edge_labels(graph.G,pos,edge_labels=labels) 
+    #abre arquivo externo
+    plt.show() 
