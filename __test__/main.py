78eae30b5a8dc9e1c2ef5617c2ea929ad0375f09
Luciano <luciano.alcantara@ufv.br>
2021-05-06 14:37:32 -0300

    finish insert pcv into graph  :boom:

--- __test__/main.py
+++ __test__/main.py
@@ -1,68 +1,29 @@
 import re
 import sys
-from math import sqrt,floor
-
-class AdjNode:
-    def __init__(self, data):
-        self.vertex = data
-        self.next = None 
- 
-# A class to represent a graph. A graph
-# is the list of the adjacency lists.
-# Size of the array will be the no. of the
-# vertices "V"
-class Graph:
-    def __init__(self, vertices):
-        self.V = vertices
-        self.graph = [None] * self.V
- 
-    # Function to add an edge in an undirected graph
-    def add_edge(self, src, dest):
-        # Adding the node to the source node
-        node = AdjNode(dest)
-        node.next = self.graph[src]
-        self.graph[src] = node
- 
-        # Adding the source node to the destination as
-        # it is the undirected graph
-        node = AdjNode(src)
-        node.next = self.graph[dest]
-        self.graph[dest] = node
- 
-    # Function to print the graph
-    def print_graph(self):
-        for i in range(self.V):
-            print("Adjacency list of vertex {}\n head".format(i), end="")
-            temp = self.graph[i]
-            while temp:
-                print(" -> {}".format(temp.vertex), end="")
-                temp = temp.next
-            print(" \n")
-
-
-# 1462,2976
-
-# É usado um regex aqui para limpar caracteres e manter apenas números
-cities_set = []
-cities_tups = []
+from math import sqrt, floor
 
 #  Abrimos o arquivo TSP e colocamos cada linha sem espaços
 #  e caracteres de nova linha em uma lista
+
 def read_tsp_data(tsp_name):
-	tsp_name = tsp_name
-	with open(tsp_name) as f:
-		content = f.read().splitlines()
-		cleaned = [x.lstrip() for x in content if x != ""]
-		return cleaned
+    tsp_name = tsp_name
+    with open(tsp_name) as f:
+        content = f.read().splitlines()
+        cleaned = [x.lstrip() for x in content if x != ""]
+        return cleaned
+
 
 """
 Verifique a linha DIMENSION no arquivo e mantenha o valor numérico
 """
+
+
 def detect_dimension(in_list):
-	non_numeric = re.compile(r'[^\d]+')
-	for element in in_list:
-		if element.startswith("DIMENSION"):
-			return non_numeric.sub("",element)
+    non_numeric = re.compile(r'[^\d]+')
+    for element in in_list:
+        if element.startswith("DIMENSION"):
+            return non_numeric.sub("", element)
+
 
 """
 Repita a lista de linhas do arquivo
@@ -71,54 +32,104 @@ gama da dimensão, mantemos o resto que são
 as coordenadas de cada cidade
 1 33.00 44.00 => 33.00 44.00
 """
-def get_cities(list,dimension):
-	dimension = int(dimension)
-	for item in list:
-		for num in range(1, dimension + 1):
-			if item.startswith(str(num)):
-				index, space, rest = item.partition(' ')
-				if rest not in cities_set:
-					cities_set.append(rest)
-	return cities_set
+
+
+def get_cities(list, dimension):
+    dimension = int(dimension)
+    array1 = []
+    for item in list:
+        for num in range(1, dimension + 1):
+            if item.startswith(str(num)):
+                index, space, rest = item.partition(' ')
+                if rest not in array1:
+                    array1.append(rest)
+    return array1
 
 
 """
 Quebra cada coordenada 33.00 44.00 para uma tupla ('33.00' ,'44.00')
 """
-def city_tup(list):
-	for item in list:
-		first_coord, space, second_coord = item.partition(' ')
-		cities_tups.append(
-            (first_coord.strip(),
-             second_coord.strip(),
-             float( sqrt( 
-                 float(first_coord.strip())*float(first_coord.strip())
-                 +
-                 float(second_coord.strip())*float(second_coord.strip())))))
-	return cities_tups
 
+
+def city_tup(list):
+    array = []
+    for item in list:
+        first_coord, space, second_coord = item.partition(' ')
+        array.append(
+            (float(first_coord.strip()), float(second_coord.strip())))
+    return array
+
+
+def tour_from_path(path):
+    """Append the first city in a path to the end in order to obtain a tour"""
+    path.append(path[0])
+    return path
+
+
+def nearest_neighbor_tour(tsp):
+    """Construct a tour through all cities in a TSP by following the nearest
+neighbor heuristic"""
+    nearest_neighbor_path = [1]
+    current_city = 1
+    cities_to_travel = set(range(2, int(detect_dimension(tsp)) + 1))
+
+    while cities_to_travel:
+        current_city = nearest_neighbor(tsp, cities_to_travel, current_city)
+        nearest_neighbor_path.append(current_city)
+        cities_to_travel.remove(current_city)
+    return tour_from_path(nearest_neighbor_path)
+
+
+def nearest_neighbor(tsp, untraveled_cities, current_city):
+    """Given a set of city keys, find the key corresponding to the
+closest city"""
+    def distance_to_current_city(city): return calc_distance(
+        tsp, current_city, city)
+    return min(untraveled_cities, key=distance_to_current_city)
+
+
+def calc_distance(tsp, city1_index, city2_index):
+    """Calculate distance between cities by their (one-based) indices"""
+    cities = city_tup(get_cities(tsp, detect_dimension(tsp)))
+    return euc_2d_distance(cities[city1_index - 1], cities[city2_index - 1])
+
+
+def euc_2d_distance(city1, city2):
+    xd = city1[0] - city2[0]
+    yd = city1[1] - city2[1]
+    return round(sqrt(xd*xd + yd*yd),2)
+
+
+def to_init_graph(tsp, list):
+    array = []
+
+    cities = city_tup(get_cities(tsp, detect_dimension(tsp)))
+
+    for item in list:
+
+        if list.index(item)<len(list)-2:
+            city1 = cities[item-1]
+            city2 = cities[(list[list.index(item)+1])-1]
+            #array.append((item, list[list.index(item)+1],city1,city2,euc_2d_distance(city1, city2)))
+            array.append((item, list[list.index(item)+1],euc_2d_distance(city1, city2)))
+        else:
+            city1 = cities[item-1]
+            city2 = cities[(list[len(list)-1])-1]
+            #array.append((item, list[list.index(item)+1], city1,city2,euc_2d_distance(city1, city2)))
+            array.append((item, list[list.index(item)+1],euc_2d_distance(city1, city2)))
+       
+    array.pop(len(array)-1)
+
+    return array
 
 """
 Juntando tudo
 """
 def produce_final(file=str(sys.argv[2])):
-	data = read_tsp_data(file)
-	dimension = detect_dimension(data)
-	cities_set = get_cities(data,dimension)
-	cities_tups = city_tup(cities_set)
-	
+    data = read_tsp_data(file)
+    return to_init_graph(data, nearest_neighbor_tour(data))
+
 
 if __name__ == '__main__':
-    produce_final()
-    print(cities_tups)
-    '''  
-     V = len(cities_tups)
-    graph = Graph(V*2)
-    
-    for i in range (len(cities_tups)):  
-        line0 = (float(cities_tups[i][0]))
-        line1 = floor(float(cities_tups[i][1]))
-        graph.add_edge(int(line0),int(line1))
-
-    graph.print_graph()
-    '''
+    res = produce_final()
+    print(res)
--- src/main.py
+++ src/main.py
@@ -331,50 +331,128 @@ def cleanFiles():
 
 
 # ------------------------------------TSP Files------------------------------------
-cities_set = []
-cities_tups = []
-
+#  Abrimos o arquivo TSP e colocamos cada linha sem espaços
+#  e caracteres de nova linha em uma lista
 
 def read_tsp_data(tsp_name):
-	tsp_name = tsp_name
-	with open(tsp_name) as f:
-		content = f.read().splitlines()
-		cleaned = [x.lstrip() for x in content if x != ""]
-		return cleaned
+    tsp_name = tsp_name
+    with open(tsp_name) as f:
+        content = f.read().splitlines()
+        cleaned = [x.lstrip() for x in content if x != ""]
+        return cleaned
+
+
+"""
+Verifique a linha DIMENSION no arquivo e mantenha o valor numérico
+"""
 
 
 def detect_dimension(in_list):
-	non_numeric = re.compile(r'[^\d]+')
-	for element in in_list:
-		if element.startswith("DIMENSION"):
-			return non_numeric.sub("", element)
+    non_numeric = re.compile(r'[^\d]+')
+    for element in in_list:
+        if element.startswith("DIMENSION"):
+            return non_numeric.sub("", element)
+
+
+"""
+Repita a lista de linhas do arquivo
+se a linha começa com um valor numérico dentro da
+gama da dimensão, mantemos o resto que são
+as coordenadas de cada cidade
+1 33.00 44.00 => 33.00 44.00
+"""
 
 
 def get_cities(list, dimension):
-	dimension = int(dimension)
-	for item in list:
-		for num in range(1, dimension + 1):
-			if item.startswith(str(num)):
-				index, space, rest = item.partition(' ')
-				if rest not in cities_set:
-					cities_set.append(rest)
-	return cities_set
+    dimension = int(dimension)
+    array1 = []
+    for item in list:
+        for num in range(1, dimension + 1):
+            if item.startswith(str(num)):
+                index, space, rest = item.partition(' ')
+                if rest not in array1:
+                    array1.append(rest)
+    return array1
 
+
+"""
+Quebra cada coordenada 33.00 44.00 para uma tupla ('33.00' ,'44.00')
+"""
 
 def city_tup(list):
-	for item in list:
-		first_coord, space, second_coord = item.partition(' ')
-		cities_tups.append(
-            (first_coord.strip(), second_coord.strip(), float(sqrt(float(first_coord.strip())*float(first_coord.strip())+float(second_coord.strip())*float(second_coord.strip())))))
-	return cities_tups
+    array = []
+    for item in list:
+        first_coord, space, second_coord = item.partition(' ')
+        array.append(
+            (float(first_coord.strip()), float(second_coord.strip())))
+    return array
 
+
+def tour_from_path(path):
+    """Append the first city in a path to the end in order to obtain a tour"""
+    path.append(path[0])
+    return path
+
+
+def nearest_neighbor_tour(tsp):
+    """Construct a tour through all cities in a TSP by following the nearest
+neighbor heuristic"""
+    nearest_neighbor_path = [1]
+    current_city = 1
+    cities_to_travel = set(range(2, int(detect_dimension(tsp)) + 1))
+
+    while cities_to_travel:
+        current_city = nearest_neighbor(tsp, cities_to_travel, current_city)
+        nearest_neighbor_path.append(current_city)
+        cities_to_travel.remove(current_city)
+    return tour_from_path(nearest_neighbor_path)
+
+
+def nearest_neighbor(tsp, untraveled_cities, current_city):
+    """Given a set of city keys, find the key corresponding to the
+closest city"""
+    def distance_to_current_city(city): return calc_distance(
+        tsp, current_city, city)
+    return min(untraveled_cities, key=distance_to_current_city)
+
+
+def calc_distance(tsp, city1_index, city2_index):
+    """Calculate distance between cities by their (one-based) indices"""
+    cities = city_tup(get_cities(tsp, detect_dimension(tsp)))
+    return euc_2d_distance(cities[city1_index - 1], cities[city2_index - 1])
+
+
+def euc_2d_distance(city1, city2):
+    xd = city1[0] - city2[0]
+    yd = city1[1] - city2[1]
+    return round(sqrt(xd*xd + yd*yd),2)
+
+
+def to_init_graph(tsp, list):
+    array = []
+
+    cities = city_tup(get_cities(tsp, detect_dimension(tsp)))
+
+    for item in list:
+
+        if list.index(item)<len(list)-2:
+            city1 = cities[item-1]
+            city2 = cities[(list[list.index(item)+1])-1]
+            #array.append((item, list[list.index(item)+1],city1,city2,euc_2d_distance(city1, city2)))
+            array.append((item, list[list.index(item)+1],euc_2d_distance(city1, city2)))
+        else:
+            city1 = cities[item-1]
+            city2 = cities[(list[len(list)-1])-1]
+            #array.append((item, list[list.index(item)+1], city1,city2,euc_2d_distance(city1, city2)))
+            array.append((item, list[list.index(item)+1],euc_2d_distance(city1, city2)))
+       
+    array.pop(len(array)-1)
+
+    return array
 
 def produce_final(file=str(sys.argv[2])):
-	data = read_tsp_data(file)
-	dimension = detect_dimension(data)
-	cities_set = get_cities(data, dimension)
-	cities_tups = city_tup(cities_set)
-
+    data = read_tsp_data(file)
+    return to_init_graph(data, nearest_neighbor_tour(data))
 # ______________________________________Driver___________________________________________
 if __name__ == "__main__":
     cleanFiles()
@@ -409,19 +487,17 @@ if __name__ == "__main__":
                     graph.add_edge(int(line['from']), int(
                         line['to']), float(line['label']))
     else:
-        produce_final()
-        V=len(cities_tups)
-        graph = Graph(V*V)
+        res = produce_final()
+        V=len(res)
+        graph = Graph(V+1)
 
-        for i in range(len(cities_tups)):
-            line0 = floor(float(cities_tups[i][0]))
-            line1 = floor(float(cities_tups[i][1]))
-            line2 = round(float(cities_tups[i][2]), 2)
-           
+        for item in res:
             try:
-                graph.add_edge(int(line0), int(line1), line2)
+                graph.add_edge(item[0], item[1], item[2])
             except:
-                print("Erro ao inserir Aresta")  
+                print("Erro ao inserir Aresta")
+
+    
     
     def option_1(): 
         arq = open("../out/saida.txt","a")       
@@ -501,11 +577,6 @@ if __name__ == "__main__":
         nx.draw_networkx_edge_labels(graph.G,pos,edge_labels=labels) 
         plt.show()
     
-    def option_10():    
-        print("Dimensão:",)
-        print(cities_tups)
-        pause()
-    
     sMenu = simpleMenu(f'{bcolors.Branco}TRABALHO GRAFOS{bcolors.Reset}')
     sMenu.spacing = [ '0','d' ]
     sMenu.menu_option_add(option_1,'Retornar a ordem do grafo')
@@ -517,5 +588,4 @@ if __name__ == "__main__":
     sMenu.menu_option_add(option_7,'Verificar se um vértice é articulação')
     sMenu.menu_option_add(option_8,'Verificar se uma aresta á ponte')
     sMenu.menu_option_add(option_9,'Visualizar o grafo')
-    sMenu.menu_option_add(option_10,'Printar TSP')
     sMenu.menu_start() 
